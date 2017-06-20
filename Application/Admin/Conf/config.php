9a27fec42aef4fdc88e32aaa563a4ceba5842d66
strays <strays_php@163.com>
2017-06-20 10:32:36 +0800

    delete repository

--- Application/Admin/Conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-	'TMPL_FILE_DEPR'      => '-',
-	'TMPL_ACTION_SUCCESS' =>'dispatch-jump',
-	'TMPL_ACTION_ERROR'   =>'dispatch-jump',
-	'DEFAULT_FILTER'      =>  ''
-);
